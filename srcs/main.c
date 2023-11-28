40c03c884bed302927a01538c0c0a53839e612ce
naton <ngobert@student.42.fr>
2023-11-29 00:51:01 +0100

    Going to bed zZz

--- srcs/main.c
+++ srcs/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  (void)argc;
   (void)argv;
 
-  printf("Hello World\n");
+  if (argc != 2) {
+    fprintf(stderr, "-------------------------------\n");
+    fprintf(stderr, " Please use only 2 arguments !\n");
+    fprintf(stderr, " Exemple : './fdf maps/42.fdf'\n");
+    fprintf(stderr, "-------------------------------\n");
+  }
 }
